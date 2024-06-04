e95b9efa705f2b6b8003433f8b06970ce56847db
yugpawar <64526473+yugpawar@users.noreply.github.com>
2024-06-04 20:33:08 +0530

    Update 1

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -4,5 +4,7 @@ set -e
 # Pull the Docker image from Docker Hub
 docker pull yugpawar/simple-python-flask-app
 
+#update
+
 # Run the Docker image as a container
 docker run -d -p 5000:5000 yugpawar/simple-python-flask-app
